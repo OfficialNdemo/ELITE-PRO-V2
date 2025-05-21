613ce20f3ca3992d5e4ce8950a869f8fca937d2c
OfficialNdemo <officialndemo@gmail.com>
2025-05-22 00:52:08 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,8 +19,8 @@ global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2\n\n+2348109263390"
 global.creator = "2348109263390@s.whatsapp.net"
-global.xprefix = '.' //set any prefix 
-global.premium = ["2348109263390"] // Premium User
+global.xprefix = '*' //set any prefix 
+global.premium = ["254741669751"] // Premium User
 //Stop here. Thanks for choosing ELITEPRO.
 
 
@@ -30,9 +30,9 @@ global.typemenu = 'v1'
 global.typereply = 'v4' 
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.welcome = true //welcome/left in groups
+global.anticall = true //bot blocks user when called
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 
